3b452e514fa078b62b2aaffb336d7d2d86ecfadb
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-01-08 10:33:10 +0100

    Seminar 2: Fix mistakes from last time

--- seminar2.rkt
+++ seminar2.rkt
@@ -46,6 +46,8 @@
   (cond [(int? e) e]
         [(true? e) e]
         [(false? e) e]
+        [(::? e) e]
+        [(empty? e) e]
         ; Convert fractions to their simplest form
         [(frac? e)
          (letrec ([v1 (mi (frac-e1 e) env)]
@@ -54,20 +56,23 @@
                (let ([factor (gcd (int-n v1) (int-n v2))])
                  (frac (/ (int-n v1) factor) (/ (int-n v2) factor)))
                (error "The elements of the fraction are not ints")))]
+        ; Flow control
         [(if-then-else? e)
-         (let ([vcond (mi (if-then-else-cnd e))])
+         (let ([vcond (mi (if-then-else-cnd e) env)])
            (if (true? vcond)
-               (mi (if-then-else-e1 e))
-               (mi (if-then-else-e2 e))))]
+               (mi (if-then-else-e1 e) env)
+               (mi (if-then-else-e2 e) env)))]
         ; Type checking
-        [(is-int? e) (if (int? (mi e env)) (true) (false))]
-        [(is-bool? e) (let ([v (mi e env)]) (if (or (true? v) (false? v)) (true) (false)))]
-        [(is-frac? e) (if (frac? (mi e env)) (true) (false))]
-        [(is-list? e) (let ([v (mi e env)]) (if (or (::? e) (empty? e)) (true) (false)))]
+        [(is-int? e) (if (int? (mi (is-int-e e) env)) (true) (false))]
+        [(is-bool? e) (let ([v (mi (is-bool-e e) env)])
+                        (if (or (true? v) (false? v)) (true) (false)))]
+        [(is-frac? e) (if (frac? (mi (is-frac-e e) env)) (true) (false))]
+        [(is-list? e) (let ([v (mi (is-list-e e) env)])
+                        (if (or (::? v) (empty? v)) (true) (false)))]
         ; Arithmetic
         [(add? e)
          (let ([v1 (mi (add-e1 e) env)]
                [v2 (mi (add-e2 e) env)])
            (cond [(and (int? v1) (int? v2)) (int (+ (int-n v1) (int-n v2)))]
                  [#t (error "The elements cannot be added")]))]
-        ))
+        [#t (false)]))
